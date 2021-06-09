951c9b5d49f0cc8d01fb2ddf18387f87b4a00090
iyasmzn <iyasmzn07@gmail.com>
2021-06-09 21:14:11 +0700

    pertama upload

---
+++ app/Http/Controllers/Latihan1Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Latihan1Controller extends Controller
+{
+    
+}
---
+++ app/Http/Controllers/LatihanController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LatihanController extends Controller
+{
+    public function index()
+    {
+        return view('latihan1');
+    }
+
+    public function create()
+    {
+        return view('latihan1');
+    }
+}
